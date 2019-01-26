6c8243720875c9dd2391c908edd05c43df4c46df
Jonathan Odgis <jonathanodgis@gmail.com>
2019-01-26 11:38:30 -0500

    Added HUD Manager for Spoons/Lives and added a Main Game Manager to handle main states and lives.

---
+++ Snow Day Global Game Jam 2019 Project/Assets/LivesCount.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesCount : MonoBehaviour
+{
+
+	public Spoon spoon;
+
+	int current_lives;
+	int current_max_lives;
+
+	public List<Spoon> spoons = new List<Spoon>(); 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MainManager.Instance == null)
+			return;
+
+		SyncLives();
+    }
+
+	void SyncLives() {
+		if (current_lives != MainManager.Instance.get_lives()) {
+			//TODO: Foreach spoon in spoon if spoon is more than the current lives. Destroy the spoon in style)
+		}
+	}
+}
---
+++ Snow Day Global Game Jam 2019 Project/Assets/MainManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+
+	public static MainManager Instance;
+
+	int lives;
+	public int MAX_LIVES_COUNT;
+
+	public enum States {
+		STARTUP, IDLE, WIN_MINIGAME, LOSE_MINIGAME, WIN_GAME, GAME_OVER
+	}
+
+	public States states;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Instance = this;
+		states = States.STARTUP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	public int get_lives() {
+		return lives;
+	}
+
+}
